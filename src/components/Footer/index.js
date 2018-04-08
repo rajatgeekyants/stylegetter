36cfa1ff4535a11fdae7c90ba8489e1795d024fb
Rajat S <rajat@geekyants.com>
2018-04-08 18:12:15 +0530

    Footer redesign

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,22 +1,76 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import './style.css';
 
 export default class Footer extends Component {
   render() {
     return (
-      <div className="footer">
-        <text className="footerText">
-          Made with
-          <div id="heart">{'\u2764'}</div>
-          by
-          <img
-            className="logo"
-            alt="GeekyAnts Logo"
-            src="https://geekyants.com/images/logo-icon-lg.png"
-          />
-          <a href="https://geekyants.com">GeekyAnts</a>
-        </text>
-      </div>
+      <Fragment>
+        <div className="footer">
+          <text className="footerText">
+            Made with
+            <div id="heart">{'\u2764'}</div>
+            by
+            <img
+              className="logo"
+              alt="GeekyAnts Logo"
+              src="https://geekyants.com/images/logo-icon-lg.png"
+            />
+            <a href="https://geekyants.com" className="GeekyAnts-link">
+              GeekyAnts
+            </a>
+          </text>
+          <div className="team-container">
+            <div className="team-mates">
+              <div className="person">
+                <div className="img-container">
+                  <img
+                    src={require('../../style/rajat.jpeg')}
+                    alt="avatar"
+                    className="image"
+                  />
+                </div>
+                <div className="name-container">
+                  <div className="firstname">Rajat</div>
+                  <div className="lastname">S</div>
+                  <div className="social">
+                    <a
+                      href="https://twitter.com/geeky_writer_"
+                      className="fa fa-twitter"
+                    />
+                    <a
+                      href="https://facebook.com/rajat.k16"
+                      className="fa fa-facebook"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="person">
+                <div className="img-container">
+                  <img
+                    src={require('../../style/Ayush.jpeg')}
+                    alt="avatar"
+                    className="image"
+                  />
+                </div>
+                <div className="name-container">
+                  <div className="firstname">Ayush</div>
+                  <div className="lastname">Bansal</div>
+                  <div className="social">
+                    <a
+                      href="https://twitter.com/geeky_writer_"
+                      className="fa fa-twitter"
+                    />
+                    <a
+                      href="https://facebook.com/rajat.k16"
+                      className="fa fa-facebook"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Fragment>
     );
   }
 }
