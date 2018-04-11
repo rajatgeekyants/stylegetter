881f6b31dc34ebd74af29517308de0c06e32771b
Ayush Bansal <ayush@geekyants.com>
2018-04-11 09:15:46 +0530

    Deployed :)

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,11 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './style.css';
 
 export default class Header extends Component {
   render() {
     return (
       <div id="header">
-        <h1>StyleGetter!</h1>
+        <h1>stylesheet.get()!!</h1>
         <h2>Paste your code and get the StyleSheet</h2>
       </div>
     );
